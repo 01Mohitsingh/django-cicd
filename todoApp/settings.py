642079426383351f9f9190b9f68f0948ecd361a2
ashutosh4146 <47911570+ashutosh4146@users.noreply.github.com>
2025-11-26 16:41:33 +0530

    Update ALLOWED_HOSTS IP address

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['65.0.181.94']
+ALLOWED_HOSTS = ['13.127.253.138']
 
 
 # Application definition
